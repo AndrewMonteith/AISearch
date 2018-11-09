9f6f8bc069b7d6e4cfd5b4d0bd0f126be1398845
AndrewMonteith <andrewmonteith1999@gmail.com>
2018-11-09 22:13:55 +0000

    Remembered existence of std::swap

--- AISearch/Swapper.cpp
+++ AISearch/Swapper.cpp
@@ -8,23 +8,17 @@ Swapper::Swapper(Graph* g)
 	indiciesDis = std::uniform_int_distribution<>(0, g->getNumberOfCities() - 1);
 }
 
-inline void swap(Tour& s, int from, int to) {
-	int temp = s[from];
-	s[from] = s[to];
-	s[to] = temp;
-}
-
 void Swapper::createSuccessor(Tour& s) {
 	from = indiciesDis(rnd);
 	to = indiciesDis(rnd);
 
-	swap(s, from, to);
+	std::swap(s[from], s[to]);
 }
 
 void Swapper::acceptSuccessor(Tour& s) {
-	swap(s, from, to);
+	std::swap(s[from], s[to]);
 }
 
 void Swapper::undo(Tour& s) {
-	swap(s, from, to);
+	std::swap(s[from], s[to]);
 }
