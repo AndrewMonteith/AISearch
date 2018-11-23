9ab47f88e005e6d47b0e708698d130f13af3f9aa
AndrewMonteith <andrewmonteith1999@gmail.com>
2018-11-23 13:33:55 +0000

    Added ranked phermeone update to ACO

--- AISearch/ACO.cpp
+++ AISearch/ACO.cpp
@@ -1,5 +1,7 @@
 #include "Aco.H"
 #include <iostream>
+#include <algorithm>
+
 std::vector<Ant> createAnts(int number, Parameters& params, Graph* g) {
 	// number = std::max(0, std::min(number, 200));
 	std::cout << "Number:" << number << std::endl;
@@ -31,6 +33,43 @@ void walkAnts(std::vector<Ant>& ants, PheremoneMatrix& matrix) {
 void resetAnts(std::vector<Ant>& ants) {
 	for (auto& ant : ants) {
 		ant.reset();
+	}
+}
+
+#include <iostream>
+
+void updatePheremonesRankBased(Graph* g, PheremoneMatrix& pheremones, Parameters& params, std::vector<Ant>& ants, int w = 30) {
+	std::vector<int> tourLengths(ants.size());
+	for (auto& ant : ants) {
+		tourLengths[ant.getId()] = g->getCostOfTour(*ant.getTour());
+	}
+
+	// Sort Ants based of length of tour.
+	std::sort(ants.begin(), ants.end(), 
+		[&](Ant& ant1, Ant& ant2) -> bool {
+			return tourLengths[ant1.getId()] < tourLengths[ant2.getId()];
+		});
+
+	std::vector<Pheremone> pheremoneUpdates(w);
+	for (auto r = 0; r < w; r++) {
+		auto ant = ants[r];
+
+		pheremoneUpdates[r] = (w-r)*params.Q / tourLengths[ant.getId()];
+	}
+
+	for (auto& d : pheremones.rawBuffer()) {
+		d *= params.EvaporationFactor;
+	}
+	
+	for (auto r = 0; r < w; r++) {
+		auto ant = ants[r];
+		auto tour = *ant.getTour();
+
+		for (auto i = 0; i < tour.size() - 1; i++) {
+			auto from = tour[i], to = tour[i + 1];
+
+			pheremones.update(from, to, pheremoneUpdates[r]);
+		}
 	}
 }
 
@@ -75,20 +114,14 @@ std::vector<int> recoverSolution(Graph* g, std::vector<Ant>& ants, PheremoneMatr
 	return *bestTour;
 }
 
-#include <iostream>
-
 std::vector<int> ACO::solve(Graph* g) {
 	auto ants = createAnts(NumberOfAnts, params, g);
-
 	auto pheremoneMatrix = createPheremoneMatrix(g, params.DefaultPheremone);
 	
-	Timer t;
 	for (auto i = 0; i < params.NumberOfIterations; i++) {
-		t.reset();
 		resetAnts(ants);
 		walkAnts(ants, pheremoneMatrix);
-		updatePheremones(g, pheremoneMatrix, params, ants);
-		std::cout << "Did Iteration " << i << " in " << t.elapsed() << " seconds" << std::endl;
+		updatePheremonesRankBased(g, pheremoneMatrix, params, ants, 50);
 	}
 
 	return recoverSolution(g, ants, pheremoneMatrix);
